8fa7d668ee7382efaacaedc57d3746c961413af2
Martin Estrada <mestra18@asu.edu>
2022-10-29 16:53:06 -0700

    Second Commit

---
+++ my-react-app/src/MyNavForm.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MyNavBar from './MyNavBar';
+import MyForm from './MyForm';
+
+export default class MyNavForm extends React.Component {
+    render(){
+    return <div><MyNavBar /><MyForm /></div>
+   
+    
+}}
